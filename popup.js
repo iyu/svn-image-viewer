1df035062b7d2185ff825d2b5f057aa1e774a769
iYU <yu.e.yu.4119@gmail.com>
2018-05-24 19:00:49 +0900

    supported audio

--- popup.js
+++ popup.js
@@ -11,13 +11,16 @@ document.getElementById('list').addEventListener('click', () => {
     for (let i = 0; i < list.length; i++) {
       const li = list[i];
       const href = li.getElementsByTagName('a')[0].getAttribute('href');
-      if (!/(\.png|\.jpg)$/.test(href)) {
-        continue;
+      if (/(\.png|\.jpg)$/.test(href)) {
+        const img = document.createElement('img');
+        img.setAttribute('src', href);
+        li.append(img);
+      } else if (/(\.mp3|\.wav|\.aac|\.ogg)$/.test(href)) {
+        const audio = document.createElement('audio');
+        audio.setAttribute('src', href);
+        audio.setAttribute('controls', 'controls');
+        li.append(audio);
       }
-
-      const img = document.createElement('img');
-      img.setAttribute('src', href);
-      li.append(img);
     }
   }
   `);
